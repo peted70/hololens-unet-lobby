96ecf46b9427a6d001127aab32b10118035a3354
Pete D <peted70@hotmail.com>
2017-06-27 19:25:51 +0100

    added some overrides in networklobbymanager

--- Assets/Scripts/CustomLobbyManager.cs
+++ Assets/Scripts/CustomLobbyManager.cs
@@ -11,11 +11,24 @@ public class CustomLobbyManager : NetworkLobbyManager
     public override void OnStartHost()
     {
         base.OnStartHost();
+        SwitchToPlayerVIew();
+    }
 
+    private void SwitchToPlayerVIew()
+    {
         // When we have started ourself as a host we need to transition to the players view..
         var go = transform.Find("MainView").gameObject;
         go.SetActive(false);
         var po = transform.Find("PlayersView").gameObject;
+        po.SetActive(true);
+    }
+
+    private void SwitchToMainVIew()
+    {
+        // When we have started ourself as a host we need to transition to the players view..
+        var go = transform.Find("PlayersView").gameObject;
+        go.SetActive(false);
+        var po = transform.Find("MainView").gameObject;
         po.SetActive(true);
     }
 
@@ -28,6 +41,11 @@ public class CustomLobbyManager : NetworkLobbyManager
         playerCollection.NodeList.Add(cn);
         playerCollection.UpdateCollection();
         return lobbyPlayer;
+    }
+
+    public override void OnLobbyStartClient(NetworkClient lobbyClient)
+    {
+        SwitchToPlayerVIew();
     }
 
     // Use this for initialization
