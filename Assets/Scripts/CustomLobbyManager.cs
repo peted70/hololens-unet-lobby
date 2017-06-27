97f2c5eacf83f208c61b3f71a9627a16bf1d92c8
Pete D <peted70@hotmail.com>
2017-06-27 19:06:17 +0100

    initial add

---
+++ Assets/Scripts/CustomLobbyManager.cs
@@ -0,0 +1,44 @@
+﻿using HUX.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomLobbyManager : NetworkLobbyManager
+{
+    public ObjectCollection playerCollection;
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+
+        // When we have started ourself as a host we need to transition to the players view..
+        var go = transform.Find("MainView").gameObject;
+        go.SetActive(false);
+        var po = transform.Find("PlayersView").gameObject;
+        po.SetActive(true);
+    }
+
+    public override GameObject OnLobbyServerCreateLobbyPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject lobbyPlayer = Instantiate(lobbyPlayerPrefab.gameObject) as GameObject;
+
+        var cn = new ObjectCollection.CollectionNode();
+        cn.transform = lobbyPlayer.transform;
+        playerCollection.NodeList.Add(cn);
+        playerCollection.UpdateCollection();
+        return lobbyPlayer;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Receivers.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HUX.Receivers;
+using HUX.Interaction;
+using HUX.Focus;
+
+public class Receivers : InteractionReceiver
+{
+    public GameObject TextObjectState;
+    public TextMesh txt;
+
+    void Start()
+    {
+        //txt = TextObjectState.GetComponentInChildren<TextMesh>();
+    }
+
+    protected override void OnTapped(GameObject obj, InteractionManager.InteractionEventArgs eventArgs)
+    {
+        //txt.text = obj.name + " : OnTapped";
+
+        switch (obj.name)
+        {
+            case "HostButton":
+                {
+                    // Not sure this is the best way to do this.. seems wrong.. 
+                    var trgt = Targets[0].GetComponent<CustomLobbyManager>();
+                    trgt.StartHost();
+                    break;
+                }
+            case "ClientButton":
+                {
+                    var trgt = Targets[0].GetComponent<CustomLobbyManager>();
+                    trgt.StartClient();
+                    break;
+                }
+        }
+        base.OnTapped(obj, eventArgs);
+    }
+
+    protected override void OnHoldStarted(GameObject obj, InteractionManager.InteractionEventArgs eventArgs)
+    {
+        //txt.text = obj.name + " : OnHoldStarted";
+        base.OnHoldStarted(obj, eventArgs);
+    }
+
+    protected override void OnFocusEnter(GameObject obj, FocusArgs args)
+    {
+        //txt.text = obj.name + " : OnFocusEnter";
+        base.OnFocusEnter(obj, args);
+    }
+
+    protected override void OnFocusExit(GameObject obj, FocusArgs args)
+    {
+        //txt.text = obj.name + " : OnFocusExit";
+        base.OnFocusExit(obj, args);
+    }
+}
